956ff1a66d35372b879e6c97bd4a406e36c98351
aless <sesdanoalessgmail.com>
2024-05-29 17:26:37 -0500

    Comit de Laboratorio 01

---
+++ app/src/main/java/pe/com/laboratorio01/ActividadPromedio.java
@@ -0,0 +1,14 @@
+package pe.com.laboratorio01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadPromedio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_promedio);
+    }
+}
